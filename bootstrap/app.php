9110a2ada363e00c38459b5f2132a84a6c10c4a0
dotLyons <dotLyonsTest@gmail.com>
2026-01-26 18:04:51 -0300

    Se corrige el HTTP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'sanctum' => EnsureFrontendRequestsAreStateful::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
